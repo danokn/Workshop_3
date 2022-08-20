0c4da08c1d53d77eba9f17650332f9d7931ce069
daniel <danokn1991@gmail.com>
2022-08-20 15:21:27 +0200

    commit

---
+++ src/main/java/workshop_3/UserList.java
@@ -0,0 +1,19 @@
+package workshop_3;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "UserList", value = "/user/list")
+public class UserList extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
